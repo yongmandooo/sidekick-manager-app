89fa712622406df4ae9877609c7354598753820b
momilsingular <swkim@nextsingular.com>
2023-08-15 10:44:01 +0900

    api request basic

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import managerAllocatedData from "./sample_data/manager-allocated.json";
 import workFinishedData from "./sample_data/work-finished.json";
 import cancelledData from "./sample_data/cancelled.json";
 import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [mode, setMode] = useState("requested");
@@ -14,7 +15,8 @@ function App() {
   const [content, setContent] = useState(null);
   let initContent = useRef(null);
 
-  useEffect(()=>{
+  useEffect(() => {
+    getCleaningSessions([1]);
     let temp = null;
     if (mode === "requested") {
       temp = requestedData;
@@ -29,23 +31,46 @@ function App() {
       return a.createTime - b.createTime;
     });
     setContent(initContent.current);
-  }, [mode]) //mode 변경마다 initContent를 초기화
+  }, [mode]); //mode 변경마다 initContent를 초기화
+
+  const getCleaningSessions = async (stateList) => {
+    await axios({
+      method: "get",
+      url: `http://localhost:8080/api/admin/cleaning_session_list`,
+      params: {
+        state_list: JSON.stringify(stateList),
+      },
+    })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   const isSameDate = (date1, date2) => {
-    return date1.getFullYear() === date2.getFullYear()
-       && date1.getMonth() === date2.getMonth()
-       && date1.getDate() === date2.getDate();
-  } //시간을 제외한 날짜 비교 함수
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  }; //시간을 제외한 날짜 비교 함수
 
-  const onTextChange = e => {
-    if(e.target.value === "" || e.target.value === null){
+  const onTextChange = (e) => {
+    if (e.target.value === "" || e.target.value === null) {
       setContent(initContent.current);
     } else {
-      setContent(initContent.current.filter(v => {
-        return (v.name.includes(e.target.value) || isSameDate(new Date(e.target.value), new Date(v.workDay)))
-      }))
+      setContent(
+        initContent.current.filter((v) => {
+          return (
+            v.name.includes(e.target.value) ||
+            isSameDate(new Date(e.target.value), new Date(v.workDay))
+          );
+        })
+      );
     }
-  } //검색 텍스트 변경마다 content 필터링
+  }; //검색 텍스트 변경마다 content 필터링
   return (
     <>
       <div className="p-8 h-screen bg-white">
@@ -120,17 +145,18 @@ function App() {
           <p className="flex-1 flex justify-center">매니저</p>
           <p className="flex-1 flex justify-center">세부 내용 확인</p>
         </div>
-        {content && content.map((item, index) => {
-          return (
-            <Request
-              key={index}
-              order={index}
-              data={item}
-              currentId={currentId}
-              setCurrentId={setCurrentId}
-            />
-          );
-        })}
+        {content &&
+          content.map((item, index) => {
+            return (
+              <Request
+                key={index}
+                order={index}
+                data={item}
+                currentId={currentId}
+                setCurrentId={setCurrentId}
+              />
+            );
+          })}
       </div>
       {currentId != null && (
         <DetailsModal
